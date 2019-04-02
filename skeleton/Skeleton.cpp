e1b8137acc7b1ed3203f136e7c1c7d4df2bb30fd
chichunchen <chichunchen844@gmail.com>
2019-04-02 14:50:49 -0400

    Add gitignore and clean up src

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,21 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Analysis/LoopPass.h"
-#include "llvm/Analysis/LoopIterator.h"
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Transforms/Utils/LoopUtils.h"
-#include "llvm/Transforms/Scalar.h"
-#include "llvm/Analysis/ScalarEvolution.h"
-#include "llvm/Analysis/ScalarEvolutionExpressions.h"
-#include "llvm/IR/Dominators.h"
-// #include "llvm/IR/TypeBuilder.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/Transforms/Utils/SimplifyIndVar.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Verifier.h"
 
 using namespace llvm;
 
