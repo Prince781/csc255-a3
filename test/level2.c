825adb4e6ebf35cf80eac0d09e1475deddc7fbe5
Prince781 <princetonf@optonline.net>
2019-04-13 23:11:07 -0400

    Adding level-{2,3} loop tests.

---
+++ test/level2.c
@@ -0,0 +1,27 @@
+void foo() {
+    int a[1000][1000];
+    int sum = 0;
+
+    for (int i = 7; i*i < 1000; ++i) {
+        for (int j = 3; j*j < 500; ++j) {
+            a[i][j] = 4;
+            sum += a[i][j-1];
+        }
+    }
+}
+
+int bar() {
+	int sum = 0;
+	for (int j = 0; j < 100; j++) {
+            for (int k = 0; k < 100; k++) {
+		sum += j;
+                sum += k;
+            }
+	}
+	return sum;
+}
+
+int main() {
+    foo();
+    bar();
+}
